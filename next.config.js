77089f76f8ae605a7e0b97f839563778f0ce423d
Mitsuru Watanabe <watanabe@zerojam.net>
2026-01-08 18:39:33 +0200

    ビルドエラーを修正

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
   },
 }
 
 module.exports = nextConfig
+
 
 
 
